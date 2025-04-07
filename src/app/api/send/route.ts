b9bdce0c2d5ba30cc8936e8b2be67cb9b393991c
clownlessmode <clownessmode@bk.ru>
2025-04-08 01:15:53 +0700

    fix

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: Request) {
     const { name, email, tel, checkbox1, checkbox2 } = body;
 
     const { error } = await resend.emails.send({
-      from: "support@vizarussia24.ru",
+      from: "support@visarussia24.ru",
       to: "eclipselucky@gmail.com",
       subject: `Новая заявка от ${name}`,
       text: `Имя: ${name}\nEmail: ${email}\nТелефон: ${tel}\nЧекбокс 1: ${checkbox1}\nЧекбокс 2: ${checkbox2}`,
